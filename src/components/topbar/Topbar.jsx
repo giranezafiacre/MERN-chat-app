e74d990873cb74e998dd219f214a8a17e57d2ae4
unknown <fiacregiraneza@gmail.com>
2022-02-01 19:24:58 +0200

    logout

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,7 @@
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import dotenv from "dotenv";
 
@@ -9,7 +9,10 @@ dotenv.config();
 export default function Topbar() {
   const { user } = useContext(AuthContext);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [show, setShow] = useState(false)
+  
   return (
+    <>
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
@@ -46,8 +49,7 @@ export default function Topbar() {
         </div>
         {/* <Link to={`/profile/${user.username}`}> */}
           <img onClick={()=>{
-            const show = JSON.parse(localStorage.getItem('show'));
-            localStorage.setItem('show',show?false:true)
+            setShow(!show)
           }}
             src={
               user.profilePicture
@@ -60,5 +62,20 @@ export default function Topbar() {
         {/* </Link> */}
       </div>
     </div>
+    {show ?
+                (<div onClick={() => {
+                    localStorage.removeItem('user');
+                    window.location.href = 'https://awesome-jennings-229f16.netlify.app/';
+                }} id='logout'>
+                    <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                        <polyline points="16 17 21 12 16 7">
+                        </polyline>
+                        <line x1="21" y1="12" x2="9" y2="12">
+                        </line>
+                    </svg>
+                </div>) : (<div></div>)}
+    </>
+    
   );
 }
--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -15,7 +15,7 @@ export default function Messenger() {
     const [newMessage, setNewMessage] = useState("");
     const [arrivalMessage, setArrivalMessage] = useState(null);
     const [onlineUsers, setOnlineUsers] = useState([])
-    const[show,setShow] = useState(false)
+    
     const socket = useRef();
     const { user } = useContext(AuthContext)
     const scrollRef = useRef();
@@ -49,10 +49,7 @@ export default function Messenger() {
             setOnlineUsers(usrs)
         })
     }, [user])
-    useEffect(() => {
-        setShow(JSON.parse(localStorage.getItem('show')))
-        console.log(show)
-    }, [show])
+   
     useEffect(() => {
         const getConversations = async () => {
             try {
@@ -109,20 +106,6 @@ export default function Messenger() {
     return (
         <>
             <Topbar />
-            {show?
-            (<div onClick={() => {
-                    localStorage.removeItem('user');
-                    localStorage.removeItem('show');
-                    window.location.href = 'https://awesome-jennings-229f16.netlify.app/';
-                }} id='logout'>
-                    <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                        <polyline points="16 17 21 12 16 7">
-                        </polyline>
-                        <line x1="21" y1="12" x2="9" y2="12">
-                        </line>
-                    </svg>
-                </div>):(<div></div>)}
             <div className="messenger">
                 <div className="chatMenu">
                     <div className="chatMenuWrapper">
